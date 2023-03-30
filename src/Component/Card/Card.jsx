f0f2276f35c95e5dbb0ebb3474c4f7edbd7ec316
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-03-31 05:47:30 +0600

    card UI design Complete

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Card.css'
 
 const Card = () => {
-    const [card, setCard] = useState();
+    const [card, setCard] = useState([]);
     console.log(card);
 
     useEffect(() => {
@@ -15,10 +15,33 @@ const Card = () => {
         <>
         <div className="card-container">
             <div className="card-left">
-                <h2>Card : {}</h2>
-                
+                <img className='cover-img' src={'https://images.unsplash.com/photo-1589149098258-3e9102cd63d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=739&q=80'} alt="" />
+                <div className="card-body">
+                    <div className="author">
+                        <div className="">
+                            <img src={'https://images.unsplash.com/profile-1626105542798-a66c7f6728a6image?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff'} alt="" />
+                        </div>
+                        <div className="author-details">
+                            <h4>Name : </h4>
+                            <p>Publish Date : </p>
+                        </div>
+                    </div>
+                    <div className="bookmark">
+                        <p className="bookmark-detail">readTime : min read</p>
+                        <button><i class="fa-regular fa-bookmark"></i></button>
+                        
+                    </div>
+                </div>
+                <h2>Blog Title : </h2>
+                <div className="keyword">
+                    <span>#beginners</span>
+                    <span>#programming</span>
+                </div>
+                <a className="read-more-btn">Mark as read</a>
             </div>
-            <div className="card-right"></div>
+            <div className="card-right">
+                <h2>Card Right</h2>
+            </div>
         </div>
         </>
     );
