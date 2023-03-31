36f5f2cfc2f103941daeed4febcce2771a0718cf
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-03-31 20:36:40 +0600

    Question & Answer Section Solved

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -31,23 +31,32 @@ const Card = ({handleReadTime, readTime, handleBookmark}) => {
         <Bookmark readTime={readTime}></Bookmark>
         </div>
         
+        {/* Question & Answer */}
         <div className="qa-container">
         <h4 className="qa-header">4 Question & Answer </h4>
         <div className="qa-content">
-            <h4>Question 1 : What is the different between Props vs state?</h4>
-            <p>Answer : Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, ipsam.</p>
+            <h4>Question 1 : What is the different between Props vs State?</h4>
+            <p>Answer : Props are passed down from a parent component to a child component as immutable data.</p>
+            <p>State is mutable data that can be changed by the component itself using the State method.</p>
         </div>
         <div className="qa-content">
             <h4>Question 2 : How does useState work?</h4>
-            <p>Answer : Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, ipsam.</p>
+            <p>Answer : useState is internal to a component and represents the component's current state. It is mutable data that can be changed by the component itself using the setState method. The state is used to manage things like user interactions, input fields, and other dynamic behavior that may change over time. State can lead to complex code and difficult-to-track bugs.</p>
         </div>
         <div className="qa-content">
             <h4>Question 3 : Purpose of useEffect other than fetching data?</h4>
-            <p>Answer : Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, ipsam.</p>
+            <p>Answer : useEffect can also be used for many other purposes</p>
+            <ul>
+              <li>Updating the document title,</li>
+              <li>Setting up event listeners,</li>
+              <li>Animating components,</li>
+              <li>Updating a global state,</li>
+              <li>Managing timers etc.</li>
+            </ul>
         </div>
         <div className="qa-content">
             <h4>Question 4 : How Does React work?</h4>
-            <p>Answer : Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, ipsam.</p>
+            <p>Answer : React is a popular JavaScript library for building user interfaces. It allows developers to create reusable UI components and efficiently update the view when the underlying data changes. Overall, React provides a powerful and efficient way to build user interfaces using a declarative and component-based approach.</p>
         </div>
       </div>
 
